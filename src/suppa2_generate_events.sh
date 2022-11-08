9a8b092a74f5cad3538b375069983e2e080c64f7
AlexWeinreb <alexis.weinreb@yale.edu>
2022-11-08 12:10:55 -0500

    Convert SUPPA2 output into tabbed SE_coords

--- src/suppa2_generate_events.sh
+++ src/suppa2_generate_events.sh
@@ -2,8 +2,8 @@
 #SBATCH --partition=general
 #SBATCH --job-name=suppa_genEvents
 #SBATCH -c 1
-#SBATCH --mem=5G
-#SBATCH --time=5:00:00
+#SBATCH --mem=1G
+#SBATCH --time=10:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alexis.weinreb@yale.edu
 
@@ -19,20 +19,30 @@ WS="WS281"
 ref_dir="/gpfs/ycga/project/ysm/hammarlund/aw853/references/$WS"
 ref_gtf=$ref_dir"/c_elegans.PRJNA13758.${WS}.canonical_geneset.gtf"
 
-out_dir="data/suppa2_data/221107_events"
+out_dir="data/suppa2_data/221108_events"
 
 mkdir -p $out_dir
 
 
+# SUPPA2 generate events
 
 suppa.py generateEvents \
     --mode DEBUG \
     --event-type SE \
     -f ioe \
     -i $ref_gtf \
-    -o $out_dir/221107_SE
+    -o $out_dir/${WS}
 
+# Reformat result for easy conversion to BED
+# From event name, extract coords of the skipped exon and the skipping intron, to tsv
 
+# could use that for headers
+#echo -e "name\tgene\tchr\tstrand\tstartLong\tendLong\tstartExon\tendExon" > SE_coords.tab
+
+cat $out_dir/${WS}_SE_strict.ioe \
+    | sed -nr 's/.*\t(WBGene[0-9]{8});SE:([IVX]{1,3}):([0-9]+)-([0-9]+):([0-9]+)-([0-9]+):([+\-])\t.*/\1\t\2\t\7\t\3\t\6\t\4\t\5/p' \
+    | awk -v'OFS=\t' '{print "SE_"NR, $0}' \
+     > $out_dir/${WS}_SE_coords.tab
 
 
 
