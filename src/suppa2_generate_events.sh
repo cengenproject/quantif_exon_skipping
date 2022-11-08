bea56a90193f5587f09e7e20dacc67fdaaea9760
Alexis Weinreb <alexis.weinreb@yale.edu>
2022-11-08 18:41:57 -0500

    Export SUPPA's annotation in simple format for use in quantif

--- src/suppa2_generate_events.sh
+++ src/suppa2_generate_events.sh
@@ -36,20 +36,19 @@ suppa.py generateEvents \
 ### Reformat result for easy conversion to BED
 # From event name, extract coords of the skipped exon and the skipping intron, to tsv
 
-# Initial version used an intermediate file, not necessary in the end, but useful 
-# for diagnostics
-# echo -e "name\tgene\tchr\tstrand\tstartLong\tendLong\tstartExon\tendExon" > SE_coords.tab
-# cat $out_dir/${WS}_SE_strict.ioe \
-#     | sed -nr 's/.*\t(WBGene[0-9]{8});SE:([IVX]{1,3}):([0-9]+)-([0-9]+):([0-9]+)-([0-9]+):([+\-])\t.*/\1\t\2\t\7\t\3\t\6\t\4\t\5/p' \
-#     | awk -v'OFS=\t' '{print "SE_"NR, $0}' \
-#      >> $out_dir/${WS}_SE_coords.tab
+# Reference of the SE events
+echo -e "name\tgene\tchr\tstrand\tstartLong\tendLong\tstartExon\tendExon" > $out_dir/${WS}_SE_coords.tab
+cat $out_dir/${WS}_SE_strict.ioe \
+    | sed -nr 's/.*\t(WBGene[0-9]{8});SE:([IVX]{1,3}):([0-9]+)-([0-9]+):([0-9]+)-([0-9]+):([+\-])\t.*/\1\t\2\t\7\t\3\t\6\t\4\t\5/p' \
+    | awk -v'OFS=\t' '{print "SE_"NR, $0}' \
+     >> $out_dir/${WS}_SE_coords.tab
 
 
 # BED file for inclusion (exonic) reads, contains coords of central exon (the one that can be skipped)
 cat $out_dir/${WS}_SE_strict.ioe \
     | sed -nr 's/.*\t(WBGene[0-9]{8});SE:([IVX]{1,3}):([0-9]+)-([0-9]+):([0-9]+)-([0-9]+):([+\-])\t.*/\1\t\2\t\7\t\3\t\6\t\4\t\5/p' \
     | awk -v'OFS=\t' '{print "SE_"NR, $0}' \
-    | awk -v'OFS="\t"' '{print($3, $7, $8, $1, 0, $4)}' \
+    | awk -v'OFS=\t' '{print($3, $7, $8, $1, 0, $4)}' \
     > $out_dir/${WS}_SE_central_exon.bed
 
 # junctions tab file to match the ones created by STAR, with the coords of the intron
